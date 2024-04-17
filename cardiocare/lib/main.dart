c32a9bc4333276f5abdcc7c07d447f13b80ef5f0
Gabriel da Silva <137402778+Gabrielsilva090@users.noreply.github.com>
2024-04-17 17:03:49 -0300

    Update de design

--- cardiocare/lib/main.dart
+++ cardiocare/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,54 +12,99 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const LoginPage(),
-        '/cadastro': (context) => const CadastroPage(),
+        '/': (context) => LoginPage(),
+        '/cadastro': (context) => CadastroPage(),
       },
     );
   }
 }
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: Text('LOGIN',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Color(0xFFF13232))),
+        bottom: PreferredSize(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Text(
+              'Olá, seja bem vindo de volta, insira seu login e senha.',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Color(0xFFF13232)),
+            ),
+          ),
+          preferredSize: Size.fromHeight(40),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Usuário',
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Usuário',
+                style: TextStyle(fontSize: 14, color: Color(0xFFF13232)),
               ),
-            ),
-            const SizedBox(height: 20.0),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Senha',
+              SizedBox(height: 10.0),
+              Container(
+                width: 200.0,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xFFDF4343)),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
               ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Implementar a lógica de autenticação aqui
-              },
-              child: const Text('Entrar'),
-            ),
-            const SizedBox(height: 20.0),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/cadastro');
-              },
-              child: const Text('Cadastrar'),
-            ),
-          ],
+              SizedBox(height: 20.0),
+              Text(
+                'Senha',
+                style: TextStyle(fontSize: 14, color: Color(0xFFF13232)),
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                width: 200.0,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xFFDF4343)),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Implementar a lógica de autenticação aqui
+                },
+                child: Text('Entrar'),
+              ),
+              SizedBox(height: 20.0),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/cadastro');
+                },
+                child: Text('Cadastrar'),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -69,58 +112,135 @@ class LoginPage extends StatelessWidget {
 }
 
 class CadastroPage extends StatelessWidget {
-  const CadastroPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cadastro'),
+        title: Text('Cadastro',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Color(0xFFF13232))),
+        bottom: PreferredSize(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Text(
+              'Preencha os campos abaixo para se cadastrar.',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Color(0xFFF13232)),
+            ),
+          ),
+          preferredSize: Size.fromHeight(40),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Nome',
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Nome',
+                style: TextStyle(fontSize: 14, color: Color(0xFFF13232)),
               ),
-            ),
-            const SizedBox(height: 20.0),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
+              SizedBox(height: 10.0),
+              Container(
+                width: 200.0,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xFFDF4343)),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(height: 20.0),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Telefone',
+              SizedBox(height: 20.0),
+              Text(
+                'Email',
+                style: TextStyle(fontSize: 14, color: Color(0xFFF13232)),
               ),
-            ),
-            const SizedBox(height: 20.0),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Senha',
+              SizedBox(height: 10.0),
+              Container(
+                width: 200.0,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xFFDF4343)),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
               ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Implementar a lógica de cadastro aqui
-              },
-              child: const Text('Cadastrar'),
-            ),
-            const SizedBox(height: 20.0),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Voltar'),
-            ),
-          ],
+              SizedBox(height: 20.0),
+              Text(
+                'Telefone',
+                style: TextStyle(fontSize: 14, color: Color(0xFFF13232)),
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                width: 200.0,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xFFDF4343)),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Senha',
+                style: TextStyle(fontSize: 14, color: Color(0xFFF13232)),
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                width: 200.0,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xFFDF4343)),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Implementar a lógica de cadastro aqui
+                },
+                child: Text('Cadastrar'),
+              ),
+              SizedBox(height: 20.0),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Voltar'),
+              ),
+            ],
+          ),
         ),
       ),
     );
