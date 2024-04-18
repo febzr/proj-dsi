cefaa2a1e6fddf9a30a7206a5d2cbd4581803dc9
Felipe Bezerra <136595371+febzr@users.noreply.github.com>
2024-04-18 09:53:01 -0300

    Refatoração de código

--- cardiocare/lib/cadastroPage.dart
+++ cardiocare/lib/cadastroPage.dart
@@ -2,107 +2,45 @@
 
 import 'package:flutter/material.dart';
 import 'home.dart';
-import 'cadastroPage.dart';
-import 'designGuide.dart';
+import 'sucessoCadastro.dart';
 
-void main() => runApp(const MyApp());
-
-final TextEditingController login = TextEditingController();
-final TextEditingController senha = TextEditingController();
 final TextEditingController nome = TextEditingController();
 final TextEditingController email = TextEditingController();
 final TextEditingController telefone = TextEditingController();
 final TextEditingController senhaCadastro = TextEditingController();
 
-const usrlogin = 'valdir';
-const senhalogin = '123';
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CardioCare',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const LoginPage(),
-        '/cadastro': (context) => const CadastroPage(),
-        '/home': (context) => const HomePage(),
-      },
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-  
-  get tituloPrincipal => DesignGuide.tituloPrincipal;
-  get subtituloPrincipal => DesignGuide.subtituloPrincipal;
+class CadastroPage extends StatelessWidget {
+  const CadastroPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
-        title: Text('LOGIN',
-            style: DesignGuide.tituloPrincipal.copyWith(fontSize: 30.0)),
+        title: const Text('CADASTRO',
+            style: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
         centerTitle: true,
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(60),
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(40),
           child: Padding(
             padding: EdgeInsets.symmetric(vertical: 8.0),
             child: Text(
-              'Olá, seja bem vindo \n de volta, insira seu login e senha.',
+              'Preencha os campos abaixo para se cadastrar.',
               textAlign: TextAlign.center,
-              style: DesignGuide.subtituloPrincipal,
+              style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
             ),
           ),
         ),
       ),
-
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(
-                'Usuário',
-                style: TextStyle(fontSize: 14, color: Colors.red.shade600),
-              ),
-
-              const SizedBox(height: 10.0),
-
-              Container(
-                width: 200.0,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.red.shade600),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: TextField(
-                    controller: login,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 20.0),
-
               const Text(
-                'Senha',
+                'Nome',
                 style: TextStyle(fontSize: 14, color: Color(0xFFF13232)),
               ),
-
               const SizedBox(height: 10.0),
               Container(
                 width: 200.0,
@@ -110,11 +48,80 @@ class LoginPage extends StatelessWidget {
                   border: Border.all(color: const Color(0xFFDF4343)),
                   borderRadius: BorderRadius.circular(5.0),
                 ),
-
                 child: Padding(
                   padding: EdgeInsets.symmetric(horizontal: 10.0),
                   child: TextField(
-                    controller: senha,
+                    controller: nome,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                'Email',
+                style: TextStyle(fontSize: 14, color: Color(0xFFF13232)),
+              ),
+              const SizedBox(height: 10.0),
+              Container(
+                width: 200.0,
+                decoration: BoxDecoration(
+                  border: Border.all(color: const Color(0xFFDF4343)),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextField(
+                    controller: email,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                'Telefone',
+                style: TextStyle(fontSize: 14, color: Color(0xFFF13232)),
+              ),
+              const SizedBox(height: 10.0),
+              Container(
+                width: 200.0,
+                decoration: BoxDecoration(
+                  border: Border.all(color: const Color(0xFFDF4343)),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextField(
+                    controller: telefone,
+                    keyboardType: TextInputType.phone,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                'Senha',
+                style: TextStyle(fontSize: 14, color: Color(0xFFF13232)),
+              ),
+              const SizedBox(height: 10.0),
+              Container(
+                width: 200.0,
+                decoration: BoxDecoration(
+                  border: Border.all(color: const Color(0xFFDF4343)),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextField(
+                    controller: senhaCadastro,
                     textAlign: TextAlign.center,
                     decoration: InputDecoration(
                       border: InputBorder.none,
@@ -123,20 +130,21 @@ class LoginPage extends StatelessWidget {
                   ),
                 ),
               ),
-
               const SizedBox(height: 20.0),
-
               ElevatedButton(
                 onPressed: () {
-                  if (login.text == usrlogin && senha.text == senhalogin) {
-                    Navigator.pushNamed(context, '/home');
+                  if (nome.text.isNotEmpty &&
+                      email.text.isNotEmpty &&
+                      telefone.text.isNotEmpty &&
+                      senhaCadastro.text.isNotEmpty) {
+                      Navigator.pushNamed(context, '/sucessoCadastro');
                   } else {
                     showDialog(
                       context: context,
                       builder: (context) {
                         return AlertDialog(
                           title: const Text('Erro'),
-                          content: const Text('Usuário ou senha inválidos.'),
+                          content: const Text('Preencha todos os campos.'),
                           actions: <Widget>[
                             TextButton(
                               onPressed: () {
@@ -156,12 +164,12 @@ class LoginPage extends StatelessWidget {
                   foregroundColor:
                       MaterialStateProperty.all<Color>(Colors.white),
                 ),
-                child: const Text('Entrar'),
+                child: const Text('Cadastrar'),
               ),
               const SizedBox(height: 20.0),
               TextButton(
                 onPressed: () {
-                  Navigator.pushNamed(context, '/cadastro');
+                  Navigator.pop(context);
                 },
                 style: ButtonStyle(
                   backgroundColor:
@@ -169,12 +177,10 @@ class LoginPage extends StatelessWidget {
                   foregroundColor:
                       MaterialStateProperty.all<Color>(Colors.white),
                 ),
-                child: const Text('Cadastrar'),
+                child: const Text('Voltar'),
               ),
-              const SizedBox(height: 20.0),
+              const SizedBox(height: 15.0),
               // Logo no final da tela
-              Positioned(
-                  bottom: 0, left: 0, right: 0, child: Text('CardioCare')),
             ],
           ),
         ),
---
+++ cardiocare/lib/designGuide.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class DesignGuide {
+  static const TextStyle tituloPrincipal = TextStyle(
+    color: Color.fromARGB(255, 0, 0, 0),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.bold
+  );
+
+  static const TextStyle subtituloPrincipal = TextStyle(
+    color: Color.fromARGB(255, 0, 0, 0),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.normal
+  );
+}
