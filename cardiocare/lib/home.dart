c89c0a0a00771f23ed34f03e12482db664822b40
Yago-Ferraz <yagomouraferraz@gmail.com>
2024-04-17 23:33:17 -0300

    home_nova

--- cardiocare/lib/home.dart
+++ cardiocare/lib/home.dart
@@ -7,7 +7,8 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HOME', style: TextStyle(color: Color(0xFFF13232))),
+        title: const Text('HOME',
+            style: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
         centerTitle: true,
         bottom: const PreferredSize(
           preferredSize: Size.fromHeight(40),
@@ -16,7 +17,7 @@ class HomePage extends StatelessWidget {
             child: Text(
               'Bem vindo ao sistema, Valdir!',
               textAlign: TextAlign.center,
-              style: TextStyle(color: Color(0xFFF13232)),
+              style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
             ),
           ),
         ),
@@ -36,6 +37,12 @@ class HomePage extends StatelessWidget {
                 onPressed: () {
                   Navigator.pop(context);
                 },
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(const Color(0xFFDF4343)),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                ),
                 child: const Text('Sair'),
               ),
             ],
