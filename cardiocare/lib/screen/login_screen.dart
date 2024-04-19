f1e106ef1f4a0ba4049c5f5a22c8632b88ca7bd6
Yago Ferraz <yagomouraferraz100@gmail.com>
2024-04-19 14:10:21 -0300

    refatoração: criando telas e componentes

---
+++ cardiocare/lib/screen/login_screen.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: camel_case_types, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+
+class login extends StatelessWidget {
+  const login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('ola')
+    );
+  }
+}
