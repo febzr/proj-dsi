e22d1642d6121577c2ce88dfefc5f41faf25af88
Felipe Bezerra <136595371+febzr@users.noreply.github.com>
2024-04-19 13:17:29 -0300

    Refatoração de Código - Parte 1

--- cardiocare/android/app/src/main/kotlin/com/example/cardiocare/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cardiocare
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- cardiocare/android/app/src/main/kotlin/com/example/dsi/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dsi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
