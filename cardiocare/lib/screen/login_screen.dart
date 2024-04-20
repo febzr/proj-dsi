4cb40ebac158ba22e1b1474a179bc78e7e97dd47
khadidjamoraes <khadidja.developer@gmail.com>
2024-04-19 23:19:08 -0300

    Update login_screen.dart

--- cardiocare/lib/screen/login_screen.dart
+++ cardiocare/lib/screen/login_screen.dart
@@ -1,15 +1,44 @@
-// ignore_for_file: camel_case_types, prefer_const_constructors
+// ignore_for_file: camel_case_types, avoid_unnecessary_containers, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:cardiocare/components/cc_textfield.dart';
+
+
 
 
 class login extends StatelessWidget {
-  const login({super.key});
-
+  
+  login({super.key});
+  
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('ola')
+    return Scaffold(
+      body:Container(
+      margin: EdgeInsets.only(top: 200),
+      child:Form(
+        key: _formKey,
+        child:Column(
+        children: [
+          cc_textfield(validador:(value){
+            if (value==null || value.isEmpty){
+              return'errou';
+            }else{
+              return null;
+            }
+          }
+          ,hint: 'oxi',label: 'oi',),
+          ElevatedButton(onPressed: (){
+            if(_formKey.currentState!.validate()){
+               ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')));
+            }
+          }
+          , child: Text('ola'))
+        ]
+      )
+    )
+    )
     );
   }
 }
