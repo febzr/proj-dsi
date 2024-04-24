5224a3de22d4fd3819f126f6375b7065597ee865
Gabriel da Silva <137402778+Gabrielsilva090@users.noreply.github.com>
2024-04-24 09:39:43 -0300

    Adição do bottomnavigationbar
    adicionado o bottonnavigationbar porém precisa refinar e irei implementar a troca de páginas e modificar mais o design

--- cardiocare_alpha/lib/screens/home.dart
+++ cardiocare_alpha/lib/screens/home.dart
@@ -9,56 +9,58 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          toolbarHeight: 180, // tamanho da appbar
-          flexibleSpace: Container(
-            color: const Color(0xFFCD1313),
-            child: Column(
-              children: [
-                SizedBox(height: 20),
-                TextField(
+        toolbarHeight: 180, // tamanho da appbar
+        flexibleSpace: Container(
+          color: const Color(0xFFCD1313),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 60,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                    child: Center(
+                      child: Text(
+                        //colocar image aqui
+                        'user',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Valdir Gomes Queiroz',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              SizedBox(height: 2),
+              SizedBox(
+                width: 400,
+                child: TextField(
                   decoration: InputDecoration(
                     hintText: 'Pesquisar',
                     fillColor: Colors.white,
                     filled: true,
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+                      borderRadius: BorderRadius.circular(15),
                       borderSide: BorderSide.none,
                     ),
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 60,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Color.fromARGB(255, 0, 0, 0),
-                      ),
-                      child: Center(
-                        child: Text(
-                          //colocar image aqui
-                          'user',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      'Valdir Gomes Queiroz',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-          backgroundColor: Colors.transparent),
+        ),
+      ),
       backgroundColor: Color.fromARGB(31, 133, 133, 133),
       body: Center(
         child: Padding(
@@ -107,6 +109,28 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xFFCD1313),
+        currentIndex: 0,
+        items: const [
+          BottomNavigationBarItem(
+            label: 'home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Diário de Pressão',
+            icon: Icon(Icons.book),
+          ),
+          BottomNavigationBarItem(
+            label: 'Remédios',
+            icon: Icon(Icons.medication_liquid),
+          ),
+          //BottomNavigationBarItem(
+          //label: 'Perfil',
+          //icon: Icon(Icons.account_circle),
+          //),
+        ],
       ),
     );
   }
